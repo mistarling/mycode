f533cb298bd455c73d02d2d18d1a495accde0495
Mike Starling <mistarling@noreply.github.com>
2022-09-06 15:39:46 -0400

    updating for conditionals

---
+++ iftest/condition01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+hostname = "MTG"
+
+def main():
+    if hostname == "MTG":
+        print("The hostname was found to be MTG")
+main()
---
+++ iftest/condition02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def main():
+    hostname = input("What value should we set for hostname?")
+    if hostname == "MTG":
+        print("The hostname was found to be MTG")
+main()
